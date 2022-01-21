7d0df32bdcbd2a6a37ce4f7b7a7d489b2525c198
kieran <Kieranrobson1999@gmail.com>
2022-01-21 11:12:24 +0000

    Add project files.

---
+++ FeedbackToolDissertation/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FeedbackToolDissertation.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FeedbackToolDissertation.Areas.Identity.IdentityHostingStartup))]
+namespace FeedbackToolDissertation.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FeedbackToolDissertationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FeedbackToolDissertationContextConnection")));
+            });
+        }
+    }
+}
