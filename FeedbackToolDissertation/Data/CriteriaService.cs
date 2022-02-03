9883a3253b43833c49c469ad847da141fa7e6b39
kieran <Kieranrobson1999@gmail.com>
2022-02-03 18:38:56 +0000

    Create CriteriaService

---
+++ FeedbackToolDissertation/Data/CriteriaService.cs
@@ -0,0 +1,34 @@
+﻿using FeedbackToolDissertation.Data.FeedbackToolDissertation;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FeedbackToolDissertation.Data
+{
+    public class CriteriaService
+    {
+        private readonly FeedbacktooldissertationContext _context;
+
+        public CriteriaService(FeedbacktooldissertationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Criteria>> GetCriteriasAsync(string strCurrentUser)
+        {
+            return
+            await _context.Criteria
+            // Only get entries for the current logged in user
+            .Where(x => x.UserName == strCurrentUser)
+            // Use AsNoTracking to disable EF change tracking
+            // Use ToListAsync to avoid blocking a thread
+            .AsNoTracking().ToListAsync();
+        }
+
+
+
+
+    }
+}
