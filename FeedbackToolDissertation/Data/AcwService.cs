e813d68be2c4b92c143e19ae8244023996beb3ed
kieran <Kieranrobson1999@gmail.com>
2022-02-03 18:36:55 +0000

    Create AcwService

---
+++ FeedbackToolDissertation/Data/AcwService.cs
@@ -0,0 +1,34 @@
+﻿using FeedbackToolDissertation.Data.FeedbackToolDissertation;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FeedbackToolDissertation.Data
+{
+    public class ACWService
+    {
+        private readonly FeedbacktooldissertationContext _context;
+
+        public ACWService(FeedbacktooldissertationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Acw>> GetAcwsAsync(string strCurrentUser)
+        {
+            return
+            await _context.Acw
+            // Only get entries for the current logged in user
+            .Where(x => x.UserName == strCurrentUser)
+            // Use AsNoTracking to disable EF change tracking
+            // Use ToListAsync to avoid blocking a thread
+            .AsNoTracking().ToListAsync();
+        }
+
+
+
+
+    }
+}
